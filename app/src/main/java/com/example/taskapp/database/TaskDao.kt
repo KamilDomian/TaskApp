9138f5e5231e97321d603bd156ebfa3410f310e1
Kamil <kamil@example.com>
2025-01-30 11:09:30 +0100

    Poprawa struktóry danych :D

--- app/src/main/java/com/example/taskapp/database/TaskDao.kt
+++ app/src/main/java/com/example/taskapp/database/TaskDao.kt
@@ -1,9 +1,13 @@
 package com.example.taskapp.database
 
+import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.taskapp.model.Task
-
+@Dao
 interface TaskDao {
     @Insert
      suspend fun insert(task : Task)
+@Query("SELECT * FROM task")
+     suspend fun getAll(): List<Task>
 }
--- app/src/main/java/com/example/taskapp/database/TaskDatabaseRepository.kt
+++ app/src/main/java/com/example/taskapp/database/TaskDatabaseRepository.kt
@@ -6,4 +6,7 @@ class TaskDatabaseRepository(private val db: AppDatabase) {
     suspend fun insertTask(task: Task){
         db.taskDao().insert(task)
     }
+    suspend fun getAllTasks(): List<Task>{
+        return db.taskDao().getAll()
+    }
 }
